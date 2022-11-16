82ac2f5a335ec5270e8ce4a45615487e03b915c5
Nicolas Jandre <nicolasjandre@live.com>
2022-11-15 22:42:05 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,56 @@
+document.getElementById('button').addEventListener('click', getVars)
+
+function close() {
+    document.getElementById('body').classList.remove('active');
+    document.getElementById('result').classList.remove('active');
+    document.getElementById('calculator').classList.remove('active');
+}
+
+function getVars() {
+    let weigth = document.getElementById('weigthInput').value
+    
+    let heigth = document.getElementById('heigthInput').value
+    heigth = heigth * 0.01
+    
+    let imc = weigth / (heigth * heigth)
+    imc = imc.toFixed(1)
+    
+    if (imc < 18.5) {
+        weigthHealth = 'magro'
+      }
+      else if (imc >= 18.5 && imc <= 24.9) {
+        weigthHealth = 'saudável'
+      }
+      else if (imc >= 25 && imc <= 29.9) {
+        weigthHealth = 'com sobrepeso'
+      }
+      else if (imc >= 30 && imc <= 39.9) {
+        weigthHealth = 'obeso'
+      }
+      else {
+        weigthHealth = 'em obesidade grave'
+      }
+    
+    document.getElementById('weigthInput').value = ""
+    document.getElementById('heigthInput').value = ""
+
+    if (isNaN(imc) || isNaN(weigth) || isNaN(heigth) || heigth === 'string' || weigth === 'string' || weigth === undefined || heigth === undefined || weigth === NaN || heigth === NaN) {
+        
+        weigth = undefined
+        heigth = undefined
+        document.getElementById('header').classList.add('active')
+        setTimeout( () => {
+            document.getElementById('header').classList.remove('active');
+        }, "4000")
+    } else {
+        document.getElementById('body').classList.add('active');
+        document.getElementById('result').classList.add('active');
+        document.getElementById('calculator').classList.add('active');
+        document.getElementById('result').innerHTML = `
+        <h2>Seu IMC é ${imc}</h2><h2>Você está ${weigthHealth}</h2>
+        <img id="close" src="./assets/close.svg" alt="">
+        `
+        document.getElementById('close').addEventListener('click', close)
+
+    }
+}
